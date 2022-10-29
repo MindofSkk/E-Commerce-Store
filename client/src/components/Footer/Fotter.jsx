79d5e0dac8a563e0e3430e3fc88fedc3b226f500
MindofSkk <helpmeskk@gmail.com>
2022-10-29 21:07:38 +0530

    work on footer part

--- client/src/components/Footer/Fotter.jsx
+++ client/src/components/Footer/Fotter.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React from "react";
+import styles from "./Footer.module.css";
 
 export const Fotter = () => {
   return (
-    <div>
-
-        all copy right 
+    <div className={styles.footer}>
+      <div className={styles.a}>
+       
+      <p>E-commerce</p>
+      </div>
+      <div className={styles.b}>
+        <ul>
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ul>
+      </div>
+      <div className={styles.c}>
+        <ul>
+          <li>Coffee</li>
+          <li>Tea</li>
+          <li>Milk</li>
+        </ul>
+      </div>
     </div>
-  )
-}
+  );
+};
